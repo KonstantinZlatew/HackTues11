f07b616ca2ff12da63e183ab8adaa3acc4ceed62
ViktorZlatev <vzlatev7@gmail.com>
2025-03-21 10:53:19 +0200

    push_notification

--- flutter/hackware/lib/main.dart
+++ flutter/hackware/lib/main.dart
@@ -6,15 +6,45 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:hackware/pages/login_page.dart';
 import 'package:hackware/pages/register_page.dart';
 import 'package:hackware/services/firebase_service.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  //maybe optional
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   GetIt.instance.registerSingleton<FirebaseService>(FirebaseService());
+
+  // Initialize Firebase Messaging
+  await setupFCM();
+
   runApp(const MyApp());
+}
+
+Future<void> setupFCM() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // Request permission for notifications
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print("✅ Notifications enabled");
+  } else {
+    print("❌ Notifications not allowed");
+  }
+
+  // Handle background messages
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+}
+
+// Background message handler
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("🌍 Background Notification: ${message.notification?.title}");
 }
 
 class MyApp extends StatelessWidget {
